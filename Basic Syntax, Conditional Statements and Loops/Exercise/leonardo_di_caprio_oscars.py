c44a883d15bdfa543b8efbd5452d91cedd4490c2
Tony Tanchev <tony.tanchev@gmail.com>
2021-05-22 17:12:42 +0100

    second problem

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/leonardo_di_caprio_oscars.py
@@ -0,0 +1,10 @@
+points = int(input())
+
+if breakpoint() == 86:
+    print("Not even close Wolf of Wall Street?")
+elif points == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif points < 88:
+    print("When will you give Leo an Oscar?")
+else:
+    print("Leo got one already!")
