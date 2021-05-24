02c9244666d87327efefcfe8877887ebad172c3e
Tony Tanchev <tony.tanchev@gmail.com>
2021-05-24 13:27:38 +0100

    from laptop

--- Basic Syntax, Conditional Statements and Loops/more exercise/sum_of_a_beach.py
+++ Basic Syntax, Conditional Statements and Loops/more exercise/sum_of_a_beach.py
@@ -1,12 +1,12 @@
 text = input()
 text = text.lower()
 
-my_list = ["sand", "water", "fish", "sun"]
+word_list = ["sand", "water", "fish", "sun"]
 counter = 0
 
-for item in my_list:
-    if item in text:
-        word_count_txt = text.count(item)
+for word in word_list:
+    if word in text:
+        word_count_txt = text.count(word)
         counter += word_count_txt
 
 print(counter)
